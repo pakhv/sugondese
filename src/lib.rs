9cb7a33b51f9efef96bb116c0abded10d8f37eed
rust-enjoyer <rust-enjoyer@gmail.com>
2023-11-04 11:37:07 +0000

    added http request parsing into struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{BufRead, BufReader, Lines, Result, Write},
+    io::{BufRead, BufReader, Read, Result, Write},
     net::{TcpListener, TcpStream},
     str::FromStr,
     time::Duration,
@@ -9,12 +9,21 @@ use crate::method_verb::MethodVerb;
 
 mod method_verb;
 
-const RESOURCE_NOT_FOUND_RESPONSE: &str = r"HTTP/1.1 404 Not Found
+const BAD_REQUEST_RESPONSE: &str = r"HTTP/1.1 400 Bad Request
 
-Resource not found";
+Bad Request";
+
+const CONTENT_LENGTH_HEADER: &str = "content-length:";
 
 pub struct WebApi<'a> {
     addr: &'a str,
+}
+
+#[derive(Debug)]
+struct HttpRequest {
+    method: MethodVerb,
+    uri: String,
+    body: Option<String>,
 }
 
 impl<'a> WebApi<'a> {
@@ -27,7 +36,8 @@ impl<'a> WebApi<'a> {
 
         for stream_result in tcp_listener.incoming() {
             if let Ok(stream) = stream_result {
-                handle_connection(stream);
+                let request = parse_request(stream);
+                println!("{:?}", request);
             }
         }
 
@@ -41,61 +51,135 @@ impl<'a> WebApi<'a> {
     }
 }
 
-fn handle_connection(stream: std::net::TcpStream) -> () {
+fn parse_request(stream: std::net::TcpStream) -> Option<HttpRequest> {
     stream
         .set_read_timeout(Some(Duration::from_secs(5)))
         .unwrap();
 
     let mut reader = BufReader::new(&stream);
-    let mut buffer = String::new();
+    let mut start_line = String::new();
 
-    if reader.read_line(&mut buffer).is_err() {
-        return_not_found(stream);
-        return;
+    if reader.read_line(&mut start_line).is_err() {
+        return_bad_request(stream);
+        return None;
     }
 
-    let verb = buffer.split_whitespace().next().unwrap_or("");
-    let uri = buffer.split_whitespace().next().unwrap_or("");
+    let mut start_line_iter = start_line.split_whitespace();
+    let verb = start_line_iter.next().unwrap_or("");
+    let uri = start_line_iter.next().unwrap_or("");
 
     let verb = MethodVerb::from_str(verb);
 
     if verb.is_err() || uri.is_empty() {
-        return_not_found(stream);
-        return;
+        return_bad_request(stream);
+        return None;
     }
 
-    let mut request_lines = reader.lines();
-    let headers = read_until_empty_string(&mut request_lines);
-    let body = read_until_empty_string(&mut request_lines);
+    let verb = verb.unwrap();
 
-    println!("{buffer}");
-    println!("{headers}");
-    println!("{body}");
+    if !could_have_body(&verb) {
+        return Some(HttpRequest {
+            method: verb,
+            uri: uri.to_string(),
+            body: None,
+        });
+    }
+
+    let headers = read_headers(&mut reader);
+
+    if headers.is_err() {
+        return_bad_request(stream);
+        return None;
+    }
+
+    let headers = headers.unwrap();
+
+    let body_length = get_content_length_header(&headers).unwrap_or(0);
+
+    if body_length == 0 {
+        return Some(HttpRequest {
+            method: verb,
+            uri: uri.to_string(),
+            body: None,
+        });
+    }
+
+    let body = read_body(&mut reader, body_length);
+
+    if body.is_err() {
+        return_bad_request(stream);
+        return None;
+    }
+    let body = body.unwrap();
+
+    return Some(HttpRequest {
+        method: verb,
+        uri: uri.to_string(),
+        body: Some(body),
+    });
 }
 
-fn read_until_empty_string(iter: &mut Lines<BufReader<&TcpStream>>) -> String {
-    let mut buffer = String::new();
+fn read_body(reader: &mut BufReader<&TcpStream>, body_length: usize) -> Result<String> {
+    let mut buffer = vec![0; body_length];
+    let mut bytes_num = 0;
 
     loop {
-        let line = iter.next().unwrap();
+        bytes_num += reader.read(&mut buffer)?;
 
-        match line {
-            Ok(line) => {
-                if line == "" {
-                    break;
-                }
-
-                buffer = format!("{buffer}\n{line}");
-            }
-            Err(_) => break,
+        if bytes_num == body_length {
+            break;
         }
     }
 
-    buffer
+    Ok(String::from_utf8(buffer).unwrap())
 }
 
-fn return_not_found(mut stream: std::net::TcpStream) -> () {
+fn get_content_length_header(headers: &String) -> Option<usize> {
+    let start_idx = headers.to_lowercase().find(CONTENT_LENGTH_HEADER);
+
+    if start_idx.is_none() {
+        return None;
+    }
+
+    let start_idx = start_idx.unwrap();
+    let end_idx = headers.get(start_idx..).unwrap().find("\n").unwrap() + start_idx;
+
+    let content_length_header = headers.get(start_idx..end_idx).unwrap();
+    let content_length = content_length_header.split(':').into_iter().last().unwrap();
+
+    match content_length.trim().parse::<usize>() {
+        Ok(length) => Some(length),
+        Err(_) => None,
+    }
+}
+
+fn read_headers(reader: &mut BufReader<&TcpStream>) -> Result<String> {
+    let mut buffer = String::new();
+    let mut current_string = String::new();
+
+    loop {
+        current_string.clear();
+        reader.read_line(&mut current_string)?;
+
+        if current_string == "\r\n" {
+            break;
+        }
+
+        buffer = format!("{buffer}{current_string}");
+    }
+
+    Ok(buffer)
+}
+
+fn return_bad_request(mut stream: std::net::TcpStream) -> () {
     stream
-        .write_all(RESOURCE_NOT_FOUND_RESPONSE.as_bytes())
+        .write_all(BAD_REQUEST_RESPONSE.as_bytes())
         .unwrap_or_else(|e| println!("{e}"));
+}
+
+fn could_have_body(method: &MethodVerb) -> bool {
+    match method {
+        MethodVerb::Get | MethodVerb::Delete => false,
+        MethodVerb::Post | MethodVerb::Put => true,
+    }
 }
